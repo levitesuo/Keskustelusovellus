bef3d62e10416dd912171951832ff4e281848a99
levitesuo <leevi.suotula@gmail.com>
2024-01-20 14:39:50 +0200

    Search working for not logged in users

--- db_modules/topics.py
+++ db_modules/topics.py
@@ -3,15 +3,15 @@ from flask import session
 from db import db
 
 def get_topics():
-    sql = """   SELECT topics.*, users.username, 
-                COUNT(posts.post_id) AS post_count,
-                MAX(posts.timestamp) AS last_post,
-                COALESCE(MAX(posts.timestamp), topics.timestamp) AS o
-                FROM topics 
-                JOIN users ON topics.owner_id = users.user_id
-                LEFT JOIN posts ON topics.topic_id = posts.topic_id
-                WHERE private_key IS NULL
-                GROUP BY topics.topic_id, users.username
+    sql = """   SELECT t.*, u.username, 
+                COUNT(p.post_id) AS post_count,
+                MAX(p.timestamp) AS last_post,
+                COALESCE(MAX(p.timestamp), t.timestamp) AS o
+                FROM topics t
+                JOIN users u ON t.owner_id = u.user_id
+                LEFT JOIN posts p ON t.topic_id = p.topic_id
+                WHERE t.private_key IS NULL
+                GROUP BY t.topic_id, u.username
                 ORDER BY o DESC"""
     result = db.session.execute(text(sql))
     t = result.fetchall()
@@ -19,21 +19,29 @@ def get_topics():
 
 def get_accessable_private_topics():
     if session["is_admin"]:
-        sql = """   SELECT topics.*, users.username, 
-                    COUNT(posts.post_id) AS post_count,
-                    MAX(posts.timestamp) AS last_post
-                    FROM topics 
-                    JOIN users ON topics.owner_id = users.user_id
-                    JOIN posts ON topics.topic_id = posts.topic_id
-                    WHERE private_key IS NOT NULL
-                    GROUP BY topics.topic_id, users.username"""
+        sql = """   SELECT t.*, u.username, 
+                    COUNT(p.post_id) AS post_count,
+                    MAX(p.timestamp) AS last_post,
+                    COALESCE(MAX(p.timestamp), t.timestamp) AS o
+                    FROM topics t
+                    JOIN users u ON t.owner_id = u.user_id
+                    LEFT JOIN posts p ON t.topic_id = p.topic_id
+                    WHERE t.private_key IS NOT NULL
+                    GROUP BY t.topic_id, u.username
+                    ORDER BY o DESC"""
         result = db.session.execute(text(sql))
     else:
-        sql = """SELECT t.*, u.username
-                FROM topics t
-                JOIN privrooms p ON t.private_key = p.private_key
-                JOIN users u ON t.owner_id = u.user_id
-                WHERE p.user_id = :user_id"""
+        sql = """   SELECT t.*, u.username, 
+                    COUNT(p.post_id) AS post_count,
+                    MAX(p.timestamp) AS last_post,
+                    COALESCE(MAX(p.timestamp), t.timestamp) AS o
+                    FROM topics t
+                    JOIN users u ON t.owner_id = u.user_id
+                    LEFT JOIN posts p ON t.topic_id = p.topic_id
+                    JOIN privrooms pr ON t.private_key = pr.private_key
+                    WHERE pr.user_id = :user_id
+                    GROUP BY t.topic_id, u.username
+                    ORDER BY o DESC"""
         result = db.session.execute(text(sql), {'user_id':session['user_id']})
     private_topics = result.fetchall()
     return private_topics
@@ -64,17 +72,29 @@ def delete_topic_by_id(id):
     db.session.commit()
 
 def get_topics_by_search(key):
-    sql = """   SELECT DISTINCT p.*, u.username 
-                FROM posts p
-                JOIN users u ON p.owner_id = u.user_id
-                JOIN topics t ON p.topic_id = t.topic_id
-                JOIN privrooms pr ON (
-                (t.private_key = pr.private_key 
-                AND pr.user_id = :user_id)
-                OR t.private_key IS NULL)
-                WHERE p.content LIKE :query
-                OR p.header LIKE :query"""
-    result = db.session.execute(text(sql),{'user_id':session["user_id"], 'query':key})   
+    if session.get("user_id"):
+        sql = """   SELECT DISTINCT p.*, u.username
+                    FROM posts p
+                    JOIN users u ON p.owner_id = u.user_id
+                    JOIN topics t ON p.topic_id = t.topic_id
+                    JOIN privrooms pr ON (
+                    (t.private_key = pr.private_key 
+                    AND pr.user_id = :user_id)
+                    OR t.private_key IS NULL)
+                    WHERE p.content LIKE :query
+                    OR p.header LIKE :query
+                    ORDER BY p.timestamp DESC"""
+        result = db.session.execute(text(sql),{'user_id':session["user_id"], 'query':key})   
+    else:
+        sql = """   SELECT DISTINCT p.*, u.username
+                    FROM posts p
+                    JOIN users u ON p.owner_id = u.user_id
+                    JOIN topics t ON p.topic_id = t.topic_id
+                    WHERE (p.content LIKE :query
+                    OR p.header LIKE :query) 
+                    AND t.private_key IS NULL
+                    ORDER BY p.timestamp DESC"""
+        result = db.session.execute(text(sql),{'query':key}) 
     stuff = result.fetchall()
     return stuff
     
